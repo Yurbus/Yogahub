e621fe5324f992ab46a0fafac1a4b7dadbbad71f
Yurbus <frankins636@gmail.com>
2025-06-04 17:24:53 +0300

    dfd

--- assets/js/swiper-slider.js
+++ assets/js/swiper-slider.js
@@ -1,7 +1,7 @@
 
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 2,
-        spaceBetween: 30,
+        spaceBetween: 20,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
