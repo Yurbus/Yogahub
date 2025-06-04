eccffc82a08ead9bf37581fb08fdaf0014056f9b
Yurbus <frankins636@gmail.com>
2025-06-04 17:04:25 +0300

    gfg

---
+++ assets/js/swiper-slider.js
@@ -0,0 +1,21 @@
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                },
+            960: {
+                slidesPerView: 2,
+            },
+        },
+    });
